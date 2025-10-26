2c83f98ea72e155427a8538463b1f238627c231e
MajidZanjani <majidmmz1972@gmail.com>
2025-10-26 14:55:15 +0400

    feat: make fetch products on page load only

--- coffee-house/src/ts/catButton.ts
+++ coffee-house/src/ts/catButton.ts
@@ -42,8 +42,14 @@ interface CartItem {
 }
 
 const user = localStorage.getItem("user");
+let allProducts: Product[] = [];
+let currentCategory: string = "coffee";
+const container: HTMLElement | null =
+  document.querySelector<HTMLElement>(".products");
 
-export function refreshCartIconCount(): void {
+window.addEventListener("resize", () => updateProductView(currentCategory));
+
+function refreshCartIconCount(): void {
   const cartNavEl = document.querySelector(".cart-el");
   const cartJSON = localStorage.getItem("cart");
   if (cartJSON) {
@@ -73,24 +79,124 @@ export function refreshCartIconCount(): void {
   }
 }
 
-export function catButton(): void {
-  let allProducts: Product[] = [];
-  const currentCategory: string = "coffee";
-  // let showingAll: boolean = false;
-
+function updateProductView(category: string): void {
   const container: HTMLElement | null =
     document.querySelector<HTMLElement>(".products");
   const btnMore: HTMLElement | null = document.getElementById(
     "btn-more"
   ) as HTMLElement;
+  if (!container) return;
+  container.innerHTML = "";
+  if (!btnMore) {
+    console.log("Error on updateProductView: btnMore not found");
+    return;
+  }
+  btnMore.style.display = "none";
+
+  const isMobile: boolean = window.innerWidth <= 768;
+  const products = allProducts.filter((p) => p.category === category);
+
+  if (isMobile && products.length > 4) {
+    renderProductList(products.slice(0, 4));
+    btnMore.style.display = "flex";
+    btnMore.onclick = (): void => {
+      // showingAll = true;
+      renderProductList(products);
+      btnMore!.style.display = "none";
+    };
+  } else {
+    renderProductList(products);
+    btnMore.style.display = "none";
+  }
+}
+
+function renderProductList(products: Product[]): void {
+  const container: HTMLElement | null =
+    document.querySelector<HTMLElement>(".products");
+  if (!container) return;
+  container.innerHTML = "";
+
+  products.forEach((p) => {
+    const productWrap = createEl("div", "product");
+    productWrap.dataset.productId = String(p.id);
+
+    const imageWrap = createEl("div", "image-wrapper");
+    const img = document.createElement("img");
+    img.src = `../img/${p.category}-${p.id}.jpg`;
+    img.alt = p.name;
+    imageWrap.appendChild(img);
+
+    const productDesc = createEl("div", "product-description");
+    const producTitle = createEl("div", "title", p.name);
+    const productDescription = createEl("div", "description", p.description);
+    const productPrice = createEl("div", "price");
+    const normalPeiceEl = createEl("div", "normal", `$${p.price}`);
+    if (p.discountPrice && user) {
+      const discountPriceEl = createEl(
+        "div",
+        "discount",
+        `$${p.discountPrice}`
+      );
+      normalPeiceEl.classList.add("strike");
+      productPrice.append(discountPriceEl);
+      productPrice.append(normalPeiceEl);
+      productDesc.append(producTitle, productDescription, productPrice);
+    } else {
+      productPrice.append(normalPeiceEl);
+      productDesc.append(producTitle, productDescription, productPrice);
+    }
+
+    productWrap.append(imageWrap, productDesc);
+    container.appendChild(productWrap);
+  });
+
+  const newList: NodeListOf<HTMLElement> =
+    container.querySelectorAll<HTMLElement>(".product");
+  newList.forEach((p: HTMLElement): void =>
+    p.addEventListener("click", (): Promise<void> => handleModalDisplay(p))
+  );
+}
+
+async function handleModalDisplay(productEl: HTMLElement): Promise<void> {
+  await fetchShow(productEl);
+}
+
+export async function catButton(): Promise<void> {
+  const loaderEl = document.querySelector(".loader");
+  currentCategory = "coffee";
+  if (loaderEl) {
+    loaderEl.classList.replace("loader-hide", "loader-show");
+    const loader = new Loader(".loader", false);
+    await loader.simulate(500);
+    loaderEl.classList.replace("loader-show", "loader-hide");
+  }
+  try {
+    const response = await fetch(
+      "https://6kt29kkeub.execute-api.eu-central-1.amazonaws.com/products"
+    );
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+    const result = await response.json();
+    if (!result) return;
+    allProducts = result.data;
+    // showingAll = false;
+    updateProductView(currentCategory);
+  } catch (err) {
+    console.error("Error loading products:", err);
+    const errorEl = createEl(
+      "div",
+      "error-element",
+      "Something went wrong. Please, refresh the page"
+    );
+    container?.appendChild(errorEl);
+  }
+
   const categoryButtons: NodeListOf<HTMLElement> =
     document.querySelectorAll<HTMLElement>(".cat-btn");
-  // const modal: HTMLElement | null = document.getElementById(
-  //   "product-modal"
-  // ) as HTMLElement;
 
   categoryButtons.forEach((button: HTMLElement): void => {
-    button.addEventListener("click", (): void => {
+    button.addEventListener("click", () => {
       if (
         button.classList.contains("active") &&
         !document.querySelector(".error-element")
@@ -103,129 +209,10 @@ export function catButton(): void {
       button.classList.add("active");
       button.classList.remove("inactive");
 
-      const categoryName: string | undefined = button.textContent
-        ?.trim()
-        .toLowerCase();
-      handleCategoryChange(categoryName);
+      currentCategory = button.textContent?.trim().toLowerCase();
+      updateProductView(currentCategory);
     });
   });
 
-  handleCategoryChange(currentCategory);
-
-  window.addEventListener("resize", updateProductView);
-
-  async function handleCategoryChange(category: string): Promise<void> {
-    if (container) container.innerHTML = "";
-
-    const loaderEl = document.querySelector(".loader");
-    if (loaderEl) {
-      loaderEl.classList.replace("loader-hide", "loader-show");
-      const loader = new Loader(".loader", false);
-      await loader.simulate(500);
-      loaderEl.classList.replace("loader-show", "loader-hide");
-    }
-
-    refreshCartIconCount();
-    try {
-      const response = await fetch(
-        "https://6kt29kkeub.execute-api.eu-central-1.amazonaws.com/products"
-      );
-      if (!response.ok) {
-        throw new Error(`HTTP error! Status: ${response.status}`);
-      }
-      const result = await response.json();
-      if (!result) return;
-      allProducts = result.data.filter(
-        (p: Product) => p.category.toLowerCase() === category
-      );
-      // showingAll = false;
-      updateProductView();
-    } catch (err) {
-      console.error("Error loading products:", err);
-      const errorEl = createEl(
-        "div",
-        "error-element",
-        "Something went wrong. Please, refresh the page"
-      );
-      container?.appendChild(errorEl);
-    }
-  }
-
-  function updateProductView(): void {
-    if (!container) return;
-    container.innerHTML = "";
-    if (!btnMore) {
-      console.log("Error on updateProductView: btnMore not found");
-      return;
-    }
-    btnMore.style.display = "none";
-
-    const isMobile: boolean = window.innerWidth <= 768;
-
-    if (isMobile && allProducts.length > 4) {
-      renderProductList(allProducts.slice(0, 4));
-      btnMore.style.display = "flex";
-      btnMore.onclick = (): void => {
-        // showingAll = true;
-        renderProductList(allProducts);
-        btnMore!.style.display = "none";
-      };
-    } else {
-      renderProductList(allProducts);
-      btnMore.style.display = "none";
-    }
-  }
-
-  function renderProductList(products: Product[]): void {
-    if (!container) return;
-    container.innerHTML = "";
-
-    products.forEach((p) => {
-      const productWrap = createEl("div", "product");
-      productWrap.dataset.productId = String(p.id);
-
-      const imageWrap = createEl("div", "image-wrapper");
-      const img = document.createElement("img");
-      img.src = `../img/${p.category}-${p.id}.jpg`;
-      img.alt = p.name;
-      imageWrap.appendChild(img);
-
-      const productDesc = createEl("div", "product-description");
-      const producTitle = createEl("div", "title", p.name);
-      const productDescription = createEl("div", "description", p.description);
-      const productPrice = createEl("div", "price");
-      const normalPeiceEl = createEl("div", "normal", `$${p.price}`);
-      if (p.discountPrice && user) {
-        const discountPriceEl = createEl(
-          "div",
-          "discount",
-          `$${p.discountPrice}`
-        );
-        normalPeiceEl.classList.add("strike");
-        productPrice.append(discountPriceEl);
-        productPrice.append(normalPeiceEl);
-        productDesc.append(producTitle, productDescription, productPrice);
-      } else {
-        productPrice.append(normalPeiceEl);
-        productDesc.append(producTitle, productDescription, productPrice);
-      }
-
-      productWrap.append(imageWrap, productDesc);
-      container.appendChild(productWrap);
-    });
-
-    const newList: NodeListOf<HTMLElement> =
-      container.querySelectorAll<HTMLElement>(".product");
-    newList.forEach((p: HTMLElement): void =>
-      p.addEventListener("click", (): Promise<void> => handleModalDisplay(p))
-    );
-  }
-
-  // async function sleep(ms: number): Promise<void> {
-  //   return new Promise((resolve) => setTimeout(resolve, ms));
-  // }
-
-  async function handleModalDisplay(productEl: HTMLElement): Promise<void> {
-    await fetchShow(productEl);
-  }
+  refreshCartIconCount();
 }
